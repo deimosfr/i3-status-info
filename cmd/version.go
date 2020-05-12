23eb33b37704514dd38f936a4f03fa5fb4d8b815
Pierre Mavro <deimos@deimos.fr>
2020-05-12 11:31:20 +0200

    feat: adding linter

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print current version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf(GetCurrentVersion())
+		fmt.Printf("%s", GetCurrentVersion())
 	},
 }
 
